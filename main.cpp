ed5787140a725b43227a131255782059e2f46173
AshleyTRS <159748745+AshleyTRS@users.noreply.github.com>
2025-01-29 14:19:00 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,7 +25,6 @@ int main()
 }
 
 bool hasNumber(wchar_t c){
-    if(c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9') return true;
-    return false;
+    reeturn (c >= '0' && c <= '9');
 }
 
